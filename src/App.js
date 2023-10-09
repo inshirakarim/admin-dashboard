250748015b4356214daa456f6e93ca58b9854352
Inshira Karim <karim.inshira@gmail.com>
2023-10-10 01:04:39 +0530

    Added sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
     <Topbar />
+    <div className="container">
+    <Sidebar />
+    </div>
+    <div className="others">
+    </div>
     </div>
   );
 }
