5099417a036f1edca61d26eca1baf6acc228da2c
Inshira Karim <karim.inshira@gmail.com>
2023-10-11 00:06:26 +0530

    Added homepage and its components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
-import Topbar from './components/topbar/Topbar';
+import Topbar from './components/topbar/Topbar'; 
+import Home from './pages/home/Home'; 
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
     <Topbar />
     <div className="container">
     <Sidebar />
-    </div>
-    <div className="others">
+    
+    <Home />
     </div>
     </div>
   );
